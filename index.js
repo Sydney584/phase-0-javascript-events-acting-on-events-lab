3c32b786df1857a3dfb2bf0913372ee30af04c36
sydney <sydneychase5841@gmail.com>
2021-03-27 20:34:20 -0400

    Done with Assignment

--- index.js
+++ index.js
@@ -12,4 +12,18 @@ function moveDodgerLeft() {
       moveDodgerLeft();
     }
   });
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left= parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
   // Your code here
